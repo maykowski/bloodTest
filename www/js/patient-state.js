b491305eab966072a58c7531f426cd022dd74252
maykowski <wojtek.maykowski@gmail.com>
2017-01-23 08:23:54 +0100

    blood test fix

---
+++ www/js/patient-state.js
@@ -0,0 +1,55 @@
+angular.module('patient-state',[])
+        .config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
+            $stateProvider
+                .state('tabs', {
+                    url: '/tab',
+                    abstract: true,
+                    templateUrl: 'templates/tabs.html',
+                    resolve: {
+                        patients: function (PatientService) {
+                            return PatientService.getPatients();
+                        }
+                    }
+                })
+
+                .state('tabs.info', {
+                    url: '/info',
+                    views: {
+                        'info-tab': {
+                            templateUrl: 'templates/info.html',
+                            controller: 'InfoController'
+                        }
+                    }
+                })
+
+                .state('tabs.list', {
+                    url: '/list',
+                    views: {
+                        'list-tab': {
+                            templateUrl: 'templates/list.html',
+                            controller: 'ListController'
+                        }
+                    }
+                })
+
+                .state('tabs.detail', {
+                    url: '/list/:aId',
+                    views: {
+                        'list-tab': {
+                            templateUrl: 'templates/detail.html',
+                            controller: 'PatientController'
+                        }
+                    }
+                })
+                .state('tabs.visit', {
+                    url: '/visit/:visitId',
+                    views: {
+                        'list-tab': {
+                            templateUrl: 'templates/visit.html',
+                            controller: 'VisitController'
+                        }
+                    }
+                });
+            $urlRouterProvider.otherwise('/tab/list');
+
+        }]);
